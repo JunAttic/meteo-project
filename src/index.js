cb629706bdee57cfd563fe056ea483d96a911b62
JunAttic <junattic@outlook.it>
2025-04-25 14:03:12 +0200

    added more things

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ function updateWeather(response) {
   let speedElement = document.querySelector("#speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
+
+  iconElement.innerHTML = `<img
+      src="${response.data.condition.icon_url}"class="temp-icon"/>`;
 
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(temperature);
